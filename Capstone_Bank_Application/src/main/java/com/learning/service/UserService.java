d5b64b6ca0a9d6286f76325b44378d457b4c36dc
Tsion-Tadesse <46496704+Tsion-Tadesse@users.noreply.github.com>
2022-11-02 00:49:41 -0400

    Added SpringSecurityConfig class and Beneficiary entity
    Tested it and runs smoothly

---
+++ Capstone_Bank_Application/src/main/java/com/learning/service/UserService.java
@@ -0,0 +1,16 @@
+package com.learning.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.learning.entity.User;
+import com.learning.repo.UserRepo;
+
+@Service
+public class UserService {
+	
+	
+
+}
