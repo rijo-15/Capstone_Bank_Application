50a22f436d48031ffab03bf97b5b22f9fb51fad5
Tsion-Tadesse <46496704+Tsion-Tadesse@users.noreply.github.com>
2022-11-08 17:19:00 -0500

    adding my changes

--- Bank_App_Front_end/BankApp/src/app/app-routing.module.ts
+++ Bank_App_Front_end/BankApp/src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { StaffloginComponent } from './stafflogin/stafflogin.component';
+import { UpdatepasswordComponent } from './updatepassword/updatepassword.component';
 
 const routes: Routes = [
   { path: 'registration', component: RegistrationComponent},
   { path: '', component: LoginComponent},
   { path: 'stafflogin', component: StaffloginComponent},
-  { path: 'adminlogin', component: AdminloginComponent}
+  { path: 'adminlogin', component: AdminloginComponent},
+  {path: 'forgotpassword', component: ForgotPasswordComponent},
+  {path: 'updatepassword', component: UpdatepasswordComponent}
 
   
 ];
