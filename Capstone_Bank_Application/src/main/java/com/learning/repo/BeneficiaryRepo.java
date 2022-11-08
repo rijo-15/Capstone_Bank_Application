500ffd9c13e97385456614c05809a3eabca96411
Rijo Varghese <rvt1515@gmail.com>
2022-11-08 11:04:34 -0500

    Revert "Merge branch 'Rijo' into master"
    This reverts commit 40092a55ba5994210fdaf3e7ef9c06f56e3b398a, reversing
    changes made to a2dbaf095cf095ad9ad009a054f81b92c3d5b249.

--- Capstone_Bank_Application/src/main/java/com/learning/repo/BeneficiaryRepo.java
+++ Capstone_Bank_Application/src/main/java/com/learning/repo/BeneficiaryRepo.java
@@ -1,16 +1,8 @@
 package com.learning.repo;
 
 import com.learning.entity.Beneficiary;
-
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface BeneficiaryRepo extends JpaRepository<Beneficiary, Long>{
-	
-	@Query(value = "Select * from Beneficiary where approved != 'yes'", nativeQuery = true)
-	 public List<Beneficiary> beneficiaryNotApproved(); 
-	
 
 }
